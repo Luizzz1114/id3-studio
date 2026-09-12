a10878f5baa5b273339b54917a39b69eba62e2e3
Luizzz1114 <cortesialuis1114@gmail.com>
2026-09-12 11:31:39 -0400

    feat: restructure Hero component layout for improved readability and consistency

--- app/components/ComoFunciona/Hero.vue
+++ app/components/ComoFunciona/Hero.vue
@@ -3,20 +3,22 @@
     aria-labelledby="hero-title"
     class="flex w-full flex-col items-center justify-between gap-10 sm:gap-12 md:flex-row md:items-center"
   >
-    <div class="flex w-full flex-1 flex-col items-start gap-6 lg:w-1/2">
-      <span class="bg-primary-500 ml-2 px-2 py-0.5 text-xs text-white uppercase">Documentación Técnica</span>
-      <h1
-        id="hero-title"
-        class="text-3d font-silkscreen m-0 flex flex-col justify-center text-[45px] leading-none font-bold text-pretty sm:text-6xl lg:text-[76px]"
-      >
-        Cómo Funciona.
-      </h1>
+    <div class="flex w-full flex-1 flex-col items-start gap-10 sm:gap-12 lg:w-1/2">
+      <div class="flex w-full flex-col items-start gap-6">
+        <span class="bg-primary-500 ml-2 px-2 py-0.5 text-xs text-white uppercase">Documentación Técnica</span>
+        <h1
+          id="hero-title"
+          class="text-3d font-silkscreen m-0 flex flex-col justify-center text-[45px] leading-none font-bold text-pretty sm:text-6xl lg:text-[76px]"
+        >
+          Cómo Funciona.
+        </h1>
+      </div>
       <div class="shadow-3d bg-default mt-2 flex w-full flex-col gap-4 border-2 p-4 text-sm sm:gap-6 sm:p-6">
         <p>
           A partir de una consulta simple (artista y canción), nuestro motor valida, extrae y cruza información de tres bases de datos distintas en milisegundos. No dependemos de un solo catálogo;
           construimos la ficha perfecta combinando la mejor información disponible para entregar un paquete de metadatos ID3 estandarizado y listo para inyectar.
         </p>
-        <div class="m-0 flex flex-wrap gap-4 sm:gap-6 p-0">
+        <div class="m-0 flex flex-wrap gap-4 p-0 sm:gap-6">
           <UButton
             to="/buscar-track"
             label="Probar una búsqueda"
